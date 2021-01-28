84674345b12727e2e1b4a9a51a404d46f7331d97
Yatao Li <yatli@microsoft.com>
2021-01-28 13:11:47 +0800

    various sleigh bug fixes

--- data/languages/env.ps1
+++ data/languages/env.ps1
@@ -1,15 +1,10 @@
 function sleigh()
 {
-  C:\Tools\ghidra_9.1.2_PUBLIC\support\sleigh.bat -e dsp56k
+  C:\ProgramData\chocolatey\lib\ghidra\tools\ghidra_9.2.1_PUBLIC\support\sleigh.bat -e dsp56k
 }
 
 function check()
 {
-  C:\Tools\ghidra_9.1.2_PUBLIC\Ghidra\Features\Decompiler\os\win64\sleigh.exe -e dsp56k
+  C:\ProgramData\chocolatey\lib\ghidra\tools\ghidra_9.2.1_PUBLIC\Ghidra\Features\Decompiler\os\win64\sleigh.exe -e dsp56k
 }
 
-function install()
-{
-  Copy-Item .\dsp56k.sla C:\Tools\ghidra_9.1.2_PUBLIC\Ghidra\Processors\dsp56k\data\languages\dsp56k.sla -Force
-  Copy-Item .\dsp56k.slaspec C:\Tools\ghidra_9.1.2_PUBLIC\Ghidra\Processors\dsp56k\data\languages\dsp56k.slaspec -Force
-}
